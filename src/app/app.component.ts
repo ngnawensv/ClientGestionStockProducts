5909579665f98fd6c4becac62af72939aa200c78
ngnawen <ngnawens@gmail.com>
2020-04-10 11:35:34 +0100

    Input and output angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ClientGestionStockProducts';
+  showHideSideBar:boolean=false;
+  onShowSideBarChange(showHideSideBar){
+
+    this.showHideSideBar=showHideSideBar;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { ProduitComponent } from './produit/produit.component';
 import { ProduitMockService } from './produit/produit-mock-service';
 import { UtilisateurComponent } from './Utilisateur/utilisateur.component';
 import { UtilisateurService } from './Utilisateur/utilisateur-mock-service';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProduitComponent,
-    UtilisateurComponent
+    UtilisateurComponent,
+    SidebarComponent,
+    ContentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
